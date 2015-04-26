4709b865acee96d144df82a70ed43a7a48e5f3f3
Teck <bboytek@gmail.com>
2015-04-26 08:00:28 +0000

    update script and add gitignore

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-$(function(){
+$(document).ready(function(){
     
-    var expand = function(e) {
-        if ($(event.target).hasClass("active-panel")) {
+    var expandPanel = function(e) {
+        if ($(e.target).hasClass("active-panel") || !$(e.target).hasClass("section") ) {
             
-            //no trigger if selected an already active panel
+            //no trigger if selected a random div or if selected an already active panel
             e.preventDefault();
             
         } else {
             
             //trigger when selected an inactive panel
             var active = $(".active-panel");
-            var target = $(event.target);
+            var target = $(e.target);
             
             //minimize previously active panel
-            active.removeClass("active-panel");
-            active.addClass("panel", 500, "easeInOutExpo");
+            active.switchClass("active-panel", "panel", 700, "easeInOutExpo");
             
             //maximize selected panel
-            target.removeClass("panel");
-            target.addClass("active-panel", 500, "easeInOutExpo");
+            target.switchClass("panel", "active-panel", 700, "easeInOutExpo");
             e.preventDefault();
             
-        };
+        }
     };
 
-    $(document).on('click', expand);
+    $(".section").on('click', expandPanel);
     
     
 });
