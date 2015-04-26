a249d838d4a78661bce64e4a46624a7ea86f58b8
Teck <bboytek@gmail.com>
2015-04-26 06:32:39 +0000

    update with preventing trigger when selecting an already active panel

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 $(function(){
     
-
-    
     var expand = function(e) {
-        //minimize previously active panel
-        $(".active-panel").toggleClass("active-panel").addClass("panel");
-        //maximize selected panel
-        $(event.target).toggleClass("panel").addClass("active-panel");
-        e.preventDefault();
+        if ($(event.target).hasClass("active-panel")) {
+            
+            //no trigger if selected an already active panel
+            e.preventDefault();
+            
+        } else {
+            
+            //trigger when selected an inactive panel
+            var active = $(".active-panel");
+            var target = $(event.target);
+            
+            //minimize previously active panel
+            active.removeClass("active-panel");
+            active.addClass("panel", 500, "easeInOutExpo");
+            
+            //maximize selected panel
+            target.removeClass("panel");
+            target.addClass("active-panel", 500, "easeInOutExpo");
+            e.preventDefault();
+            
+        };
     };
-    
-    
+
     $(document).on('click', expand);
     
     
